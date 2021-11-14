17b0ec4e656e3b573eb7cd80969b8fd0e76fc8ad
Ravi <34369761+kavinotravi@users.noreply.github.com>
2021-11-14 13:07:09 +0530

    Added Documentation for MPI Process Class (#36)
    * Added Documentation for MPI Process Class
    
    * Updated documentation for mpi_process.py
    
    Co-authored-by: Ravi <kavinotravi@github.com>

--- fedrec/multiprocessing/mpi_process.py
+++ fedrec/multiprocessing/mpi_process.py
@@ -8,7 +8,18 @@ from fedrec.utilities import registry
 
 @registry.load("multiprocessing", "MPI")
 class MPIProcess:
+    """
+    Construct an MPI Process Manager for Trainers
 
+    Attributes
+    ----------
+    trainer : BaseTrainer
+        Trainer executing on the actor
+    logger : logger
+        Logger Object
+    com_manager_config : dict
+        Communication of config manager stored as dictionary
+    """
     def __init__(self,
                  worker: BaseActor,
                  logger,
@@ -18,6 +29,13 @@ class MPIProcess:
             "communications", config_dict=com_manager_config)
 
     def run(self) -> None:
+        """
+        After calling the function, the Communication 
+        Manager listens to the queue for messages, 
+        executes the job request and publishes the results 
+        in that order. It will stop listening after receiving
+        job_request with job_type "STOP" 
+        """
         while True:
             job_request: JobSubmitMessage = self.process_comm_manager.receive_message()
             if job_request.job_type == "STOP":
@@ -27,6 +45,9 @@ class MPIProcess:
             self.publish(result)
 
     def publish(self, job_result: JobResponseMessage) -> None:
+        """
+        Publishes the result after executing the job request
+        """
         self.process_comm_manager.send_message(job_result.result())
 
     def stop(self) -> None:
